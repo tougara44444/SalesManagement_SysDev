1653c54e300265daa675a3312d7e8dcfffd2bd5c
Yokoyama Momiji <c2369@oic.jp>
2023-10-27 13:52:11 +0900

    ロケーション変更

--- SalesManagement_SysDev/F_login.Designer.cs
+++ SalesManagement_SysDev/F_login.Designer.cs
@@ -71,7 +71,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(544, 56);
+            this.label1.Location = new System.Drawing.Point(556, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(257, 40);
             this.label1.TabIndex = 0;
